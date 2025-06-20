51afced35b99034d49c4e092e1c9c3d4a9720609
plasma-effect <mk_vertex3625_plasma@kcn.jp>
2025-06-20 16:18:28 +0900

    rename lazy_segtree arg

--- include/segtree.hpp
+++ include/segtree.hpp
@@ -10,8 +10,9 @@ template <typename T> using add_segtree = segtree<numeric::plus<T>>;
 template <typename T> using mul_segtree = segtree<numeric::multiplies<T>>;
 template <typename T> using max_segtree = segtree<numeric::max<T>>;
 
-template <auto m0, auto m1>
+template <auto monoid, auto mapping>
 using lazy_segtree =
-    atcoder::lazy_segtree<numeric::internal::value_t<m0>, m0, m0,
-                          numeric::internal::value_t<m1>, m1, m1, m1>;
+    atcoder::lazy_segtree<numeric::internal::value_t<monoid>, monoid, monoid,
+                          numeric::internal::value_t<mapping>, mapping, mapping,
+                          mapping>;
 } // namespace common
