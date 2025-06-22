d88b3d743d6eaee810dfb9f9cdf50fbcce557633
plasma-effect <mk_vertex3625_plasma@kcn.jp>
2025-06-22 23:48:13 +0900

    remove common::abs

--- include/math.hpp
+++ include/math.hpp
@@ -3,13 +3,6 @@
 #include "utility.hpp"
 
 namespace common {
-template <std::integral T> constexpr T abs(T const& v) {
-  if constexpr (std::is_signed_v<T>) {
-    return v < 0 ? -v : v;
-  } else {
-    return v;
-  }
-}
 template <typename T> std::vector<T> make_factorial_array(std::size_t max) {
   std::vector<T> ret(max + 1);
   ret[0] = T(1);
--- unittest/math.cpp
+++ unittest/math.cpp
@@ -2,29 +2,6 @@
 #include "atcoder/all"
 #include <gtest/gtest.h>
 
-TEST(Math, Abs) {
-  static_assert(
-      std::is_same_v<decltype(common::abs(std::declval<int>())), int>);
-  static_assert(
-      std::is_same_v<decltype(common::abs(std::declval<std::int64_t>())),
-                     std::int64_t>);
-  static_assert(
-      std::is_same_v<decltype(common::abs(std::declval<std::uint64_t>())),
-                     std::uint64_t>);
-  static_assert(
-      std::is_same_v<decltype(common::abs(std::declval<std::size_t>())),
-                     std::size_t>);
-  EXPECT_EQ(common::abs(1), 1);
-  EXPECT_EQ(common::abs(3), 3);
-  EXPECT_EQ(common::abs(-1), 1);
-  EXPECT_EQ(common::abs(-3), 3);
-  EXPECT_EQ(common::abs(0), 0);
-
-  EXPECT_EQ(common::abs(0u), 0u);
-  EXPECT_EQ(common::abs(1u), 1u);
-  EXPECT_EQ(common::abs(3u), 3u);
-}
-
 TEST(Math, FactorialInt) {
   auto fact = common::make_factorial_array<int>(5);
   ASSERT_EQ(fact.size(), 6);
