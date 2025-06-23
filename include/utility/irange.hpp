97e7e33501ffbb96bfaa5aa30830f164713f5ba0
plasma-effect <mk_vertex3625_plasma@kcn.jp>
2025-06-23 13:23:55 +0900

    add dual_irange

--- include/utility/irange.hpp
+++ include/utility/irange.hpp
@@ -9,4 +9,53 @@ template <std::integral Int> auto irange(Int first, Int last) {
 template <std::integral Int> auto irange(Int last) {
   return irange(Int(0), last);
 }
+
+namespace internal {
+template <std::integral Int> struct dual_irange_t {
+  Int f0, f1, e0, e1;
+  struct iterator {
+    using difference_type = Int;
+    using value_type = std::pair<Int, Int>;
+    using iterator_concept = std::input_iterator_tag;
+    Int f0, f1, e0, e1;
+    Int v0, v1;
+    auto operator*() const {
+      return std::pair(v0, v1);
+    }
+    auto& operator++() {
+      if (++v1 == e1) {
+        v1 = f1;
+        ++v0;
+      }
+      return *this;
+    }
+    auto operator++(int) {
+      auto ret = *this;
+      if (++v1 == e1) {
+        v1 = f1;
+        ++v0;
+      }
+      return ret;
+    }
+
+    bool operator==(std::default_sentinel_t) const {
+      return v0 == e0;
+    }
+  };
+  auto begin() const {
+    return iterator{f0, f1, e0, e1, f0, f1};
+  }
+  auto end() const {
+    return std::default_sentinel;
+  }
+};
+} // namespace internal
+template <std::integral Int> auto dual_irange(Int f0, Int e0, Int f1, Int e1) {
+  CL_ASSERT(std::cmp_less_equal(f0, e0));
+  CL_ASSERT(std::cmp_less_equal(f1, e1));
+  return internal::dual_irange_t{f0, f1, e0, e1};
+}
+template <std::integral Int> auto dual_irange(Int e0, Int e1) {
+  return dual_irange(Int(0), e0, Int(0), e1);
+}
 } // namespace common
--- unittest/utility/irange.cpp
+++ unittest/utility/irange.cpp
@@ -15,3 +15,33 @@ TEST(Utility, IrangeReverse) {
   std::vector vec = {4, 3, 2, 1, 0};
   ASSERT_TRUE(std::ranges::equal(vec, common::irange(5) | std::views::reverse));
 }
+
+TEST(Utility, DualIrange2Args) {
+  std::vector<std::pair<int, int>> vec = {
+      {0, 0},
+      {0, 1},
+      {0, 2},
+      {1, 0},
+      {1, 1},
+      {1, 2},
+      {2, 0},
+      {2, 1},
+      {2, 2},
+  };
+  ASSERT_TRUE(std::ranges::equal(vec, common::dual_irange(3, 3)));
+}
+
+TEST(Utility, DualIrange4Args) {
+  std::vector<std::pair<int, int>> vec = {
+      {2, 1},
+      {2, 2},
+      {2, 3},
+      {3, 1},
+      {3, 2},
+      {3, 3},
+      {4, 1},
+      {4, 2},
+      {4, 3},
+  };
+  ASSERT_TRUE(std::ranges::equal(vec, common::dual_irange(2, 5, 1, 4)));
+}
