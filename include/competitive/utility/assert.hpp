408b25d14ffb4bf6a1e483e26c20b2bb465ed35a
plasma-effect <mk_vertex3625_plasma@kcn.jp>
2025-08-12 00:11:30 +0900

    fix assertion message

--- include/competitive/utility/assert.hpp
+++ include/competitive/utility/assert.hpp
@@ -5,10 +5,13 @@
 namespace competitive::debug::internal {
 [[noreturn]] inline void assertion_fail(const char* expr) {
   auto st = std::stacktrace::current(1);
-  std::cerr << "assertion failed: \"" << expr << "\"\nwhere: " << st[0] << "\n";
+  std::cerr << "assertion failed: \"" << expr << "\"\n";
+  std::cerr << "where: " << st[0].description();
+  std::cerr << ": " << st[0].source_line() << "\n";
   for (auto it = std::next(st.begin()); it != st.end(); ++it) {
     if (it->source_line() != 0) {
-      std::cerr << " from: " << *it << "\n";
+      std::cerr << " from: " << it->description();
+      std::cerr << ": " << it->source_line() << "\n";
     }
   }
   std::cerr << std::flush;
